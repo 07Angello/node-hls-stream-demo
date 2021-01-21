e1b0f059108f8af5ab467b9c8241e4083354ed53
Angello Antonelly <angelloantonelly@live.com>
2021-01-21 14:26:40 -0600

    made changes to implement file system

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+// file system
+const fs = require('fs');
 const app = express();
+
+app.use( express.static('public') );
+
+app.use( express.json() );
 
 app.listen(4000, () => {
     console.log('Server running on port 4000');
